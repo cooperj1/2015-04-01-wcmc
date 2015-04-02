f87e644d0c708e3af7ade1ee278872635bc29b43
Jon Cooper <jonathan.eric.cooper@gmail.com>
2015-04-02 10:11:49 -0400

    function to convert f_to_c

--- src/temp_convert.py
+++ src/temp_convert.py
@@ -2,4 +2,9 @@ def f_to_K(temp):
 	return ((temp - 32) * (5 /float(9))) + 273.15
 
 def k_to_c(temp):
-	return (temp) + 273.15)
+	return temp - 273.15
+
+def f_to_c(temp):
+	temp_k = f_to_k(temp)
+	result = k_to_c(temp_k)
+	return(result)
